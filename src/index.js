e82775671d752792b0613082045990f5ab6f187c
Rochelle Anderson <Rochellebrandy@icloud.com>
2024-07-02 17:49:46 +0100

    added AI API contexts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(Response) {
   new Typewriter("#poem", {
-    strings: "Your answer herrrrr",
+    strings: Response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 1,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "54a0b1t705328c1ff5aabef7485od2a3";
+  let context =
+    "You are a romantic AI writer. You write 4 line answers in HTML format eg. <p> Your answer</p>. Sign the end of the poem within <strong>'Yours Sincerely'</strong> Not the beginning";
+  let prompt =
+    "User instructions: Always Rengenerate a new short and unique poem about ${instructionsInput.value}";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Generating Poem");
+  console.log("Context: ${context}");
+  console.log("Prompt: ${prompt}");
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
