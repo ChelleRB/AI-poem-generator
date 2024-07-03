b65c89c8cd3430558885b994a9b05a7403d02712
Rochelle Anderson <Rochellebrandy@icloud.com>
2024-07-03 23:29:19 +0100

    small change

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "54a0b1t705328c1ff5aabef7485od2a3";
   let context =
-    "You are a romantic AI writer. You write 4 line answers in HTML format eg. <p> Your answer</p>. Sign the end of the poem within <strong>'Yours Sincerely'</strong> Not the beginning";
+    "You are a creative AI writer. You write 4 line answers in HTML format eg. <p> Your answer</p>. Sign the end of the poem within <strong>'Yours Sincerely'</strong> Not the beginning";
   let prompt =
     "User instructions: Always Rengenerate a new short and unique poem about ${instructionsInput.value}";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
